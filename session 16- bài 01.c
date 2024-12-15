dd6a17fff0fec5888311f99ac7e0f651f04fba55
quangduymanh <quangduymanh4@gmail.com>
2024-12-16 01:14:53 +0700

    upload bài 01

---
+++ session 16- bài 01.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a= 20;
+	int *ptr=&a;
+	printf("gia tri cua a duoc khai bao bang bien la %d\n",a);
+	printf("dia chi cua a sau khi duoc khai bao bang bien: %p\n",(void*)&a);
+	printf("gia tri cua a sau khi duoc khai bao bang con tro la %d\n",*ptr);
+	printf("dia chi cua a sau khi duoc khai bao bang con tro la: %p\n",(void*)ptr);
+	
+	
+    return 0;
+}
